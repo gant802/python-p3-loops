d9453f292ecc8c192aa417a15acdf67e076af033
Grant Cummings <grantcummings4429@gmail.com>
2024-05-13 14:34:54 -0400

    finished lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
+import ipdb
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i != 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!") 
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    doubled = [number ** 2 for number in int_list]
+    return doubled
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    
+    for q in range(1, 101):
+        if q % 3 == 0 and q % 5 == 0: 
+            print("FizzBuzz")
+        elif q % 3 == 0:
+            print("Fizz")
+        elif q % 5 == 0:
+            print("Buzz")
+        else : 
+            print(q)
+            
+   
+
+
+fizzbuzz()
